71d21f5158c490036948dcce52b54b86da683498
Davin Cheong <128515113+WhiteMochi27@users.noreply.github.com>
2024-10-17 08:19:38 +0000

    bgp

--- main.py
+++ main.py
@@ -102,3 +102,17 @@ def main():
 
 if __name__ == "__main__":
     main()
+
+st.markdown("""
+    <style>
+        body {
+            margin: 0;
+            padding: 0;
+            height: 100vh;
+            background-image: url('helloworld.jpg');
+            background-size: cover;
+            background-position: center;
+            background-repeat: no-repeat;
+        }
+    <style>
+""")
